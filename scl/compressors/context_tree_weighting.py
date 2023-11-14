c8d2895e35e33606f698cb0b4a42dc23a44510cb
Sean Shao <sean.shao8253@gmail.com>
2023-11-14 00:26:25 -0800

    Convert usage of strings to BitArray.

--- scl/compressors/context_tree_weighting.py
+++ scl/compressors/context_tree_weighting.py
@@ -1,14 +1,9 @@
 from __future__ import annotations
+from scl.utils.bitarray_utils import BitArray, uint_to_bitarray
 from scl.utils.tree_utils import BinaryNode
 from typing import Literal
 import copy
 import numpy as np
-
-def is_valid_binary_string(binary_string: str) -> bool:
-    for char in binary_string:
-        if char != "0" and char != "1":
-            return False
-    return True
 
 class CTWNode(BinaryNode):
     """represents a node of the CTW tree
@@ -22,20 +17,20 @@ class CTWNode(BinaryNode):
     kt_prob: float = 1      # represents kt probability for current a, b
     node_prob: float = 1    # probability of node
 
-    def get_child(self, symbol: Literal["0", "1"]) -> CTWNode:
-        if symbol == "0":
+    def get_child(self, symbol: bool) -> CTWNode:
+        if not symbol:
             return self.left_child
         else:
             return self.right_child
 
-    def get_count(self, symbol: Literal["0", "1"]) -> int:
-        if symbol == "0":
+    def get_count(self, symbol: bool) -> int:
+        if not symbol:
             return self.a
         else:
             return self.b
 
-    def increment_count(self, symbol: Literal["0", "1"]):
-        if symbol == "0":
+    def increment_count(self, symbol: bool):
+        if not symbol:
             self.a += 1
         else:
             self.b += 1
@@ -43,7 +38,7 @@ class CTWNode(BinaryNode):
     def pr_prob(self, nx: int, n: int, alpha: float) -> float:
         return (nx + alpha) / (n + 1)
 
-    def kt_update(self, next_symbol: Literal["0", "1"], alpha: float):
+    def kt_update(self, next_symbol: bool, alpha: float):
         nx = self.get_count(next_symbol)
         n = self.a + self.b
         self.kt_prob = self.kt_prob * self.pr_prob(nx=nx, n=n, alpha=alpha)
@@ -66,31 +61,29 @@ class CTWNode(BinaryNode):
 class CTWTree():
 
     root: CTWNode = None
-    current_context: str = None
+    current_context: BitArray = None
     snapshot: list = None
     get_snapshot: bool = None
     alpha: float = 0.5
 
-    def __init__(self, tree_height: int, past_context: str):
-        assert is_valid_binary_string(past_context)
+    def __init__(self, tree_height: int, past_context: BitArray):
         assert len(past_context) == tree_height
 
-        self.root = self.gen_tree(tree_height, "")
+        self.root = self.gen_tree(depth=tree_height, node_context=BitArray())
 
         self.current_context = past_context
 
     def print_tree(self):
         self.root.print_node()
 
-    def gen_tree(self, depth: int, current_context: str) -> CTWNode:
+    def gen_tree(self, depth: int, node_context: BitArray) -> CTWNode:
         if depth == 0:
-            return CTWNode(id=current_context, left_child=None, right_child=None)
-        left_child = self.gen_tree(depth-1, current_context + "0")
-        right_child = self.gen_tree(depth-1, current_context + "1")
-        return CTWNode(id=current_context, left_child=left_child, right_child=right_child)
+            return CTWNode(id=node_context, left_child=None, right_child=None)
+        left_child = self.gen_tree(depth=depth-1, node_context=node_context + BitArray(0))
+        right_child = self.gen_tree(depth=depth-1, node_context=node_context + BitArray(1))
+        return CTWNode(id=node_context, left_child=left_child, right_child=right_child)
     
-    def update_tree(self, sequence: str):
-        assert is_valid_binary_string(sequence)
+    def update_tree(self, sequence: BitArray):
         for symbol in sequence:
             self._update_tree_symbol(symbol)
 
@@ -104,7 +97,7 @@ class CTWTree():
             node.node_prob = prev_state.node_prob
         self.snapshot = []
 
-    def get_symbol_prob(self, symbol: Literal["0", "1"]):
+    def get_symbol_prob(self, symbol: bool):
         self.snapshot = []
         self.get_snapshot = True
         context_prob = self.root.node_prob
@@ -115,11 +108,11 @@ class CTWTree():
         self.get_snapshot = False
         return symbol_prob
 
-    def _update_tree_symbol(self, next_symbol: Literal["0", "1"]):
+    def _update_tree_symbol(self, next_symbol: bool):
         self._update_node(node=self.root, context=self.current_context, symbol=next_symbol)
-        self.current_context = self.current_context[1:] + next_symbol
+        self.current_context = self.current_context[1:] + uint_to_bitarray(next_symbol)
 
-    def _update_node(self, node: CTWNode, context: str, symbol: str):
+    def _update_node(self, node: CTWNode, context: str, symbol: bool):
         if len(context) == 0:
             if self.get_snapshot:
                 self.snapshot.append((node, copy.deepcopy(node)))
@@ -135,29 +128,29 @@ def test_ctw_node():
     pass
 
 def test_ctw_tree_generation():
-    test = CTWTree(tree_height=3, past_context="110")
+    test = CTWTree(tree_height=3, past_context=BitArray("110"))
     np.testing.assert_almost_equal(
         test.root.node_prob,
         1,
     )
 
-    test.update_tree("0100110")
+    test.update_tree(BitArray("0100110"))
     np.testing.assert_almost_equal(
         test.root.node_prob,
         7/2048,
     )
 
-    test.update_tree("0")
+    test.update_tree(BitArray("0"))
     np.testing.assert_almost_equal(
         test.root.node_prob,
         153/65536,
     )
 
 def test_ctw_tree_probability():
-    test = CTWTree(tree_height=3, past_context="110")
-    test.update_tree("0100110")
+    test = CTWTree(tree_height=3, past_context=BitArray("110"))
+    test.update_tree(BitArray("0100110"))
     np.testing.assert_almost_equal(
-        test.get_symbol_prob("0"),
+        test.get_symbol_prob(0),
         (153/65536)/(7/2048)
     )
     np.testing.assert_almost_equal(
@@ -165,6 +158,6 @@ def test_ctw_tree_probability():
         7/2048,
     )
     np.testing.assert_almost_equal(
-        test.get_symbol_prob("1"),
+        test.get_symbol_prob(1),
         (71/65536)/(7/2048)
     )
